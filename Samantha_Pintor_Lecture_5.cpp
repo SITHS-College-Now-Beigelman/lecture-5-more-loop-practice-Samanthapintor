f3d30f23eb036c8a9b0e3ab94a0eadb94351f43a
Samanthapintor <samanthap329@nycstudents.net>
2024-10-07 20:40:12 -0400

    Update Samantha_Pintor_Lecture_5.cpp

--- Samantha_Pintor_Lecture_5.cpp
+++ Samantha_Pintor_Lecture_5.cpp
@@ -3,70 +3,61 @@
 //Samantha Pintor
 //More Loops Practice 
 //10-07-24
-
 #include <iostream> 
+#include <iomanip>
+#include <string> 
 
 using namespace std; 
 
 int main ()
 { 
-    //naming variables 
-    float bankMoney; //variable for bank money amount
-    int numberTransactions; //variable for number of transactions
-    float amountTransaction; //variable for amount per transaction
-    int transactionType; //variable for transaction type - debit or credit
-    int yesOrNo; //variable to continue or not 
-    float totalDebit = (bankMoney - amountTransaction); //variable for total debit
-    float totalCredit = (bankMoney + amountTransaction); //variable for total credit
+    // Naming variables 
+    float startAmount; // Variable for bank money amount
+    float bankMoney1; // Current bank money after transactions w/ debit
+    float bankMoney2; // Current bank money after transactions w/ credit
+    int numberTransactions; // Variable for number of transactions
+    float amountTransaction; // Variable for amount per transaction
+    int transactionType; // Variable for transaction type - debit or credit
+    int a = 0; // Initialize counter
+    float totalMoney; //total money at end of day
 
-    //asking user
-    cout << "Hello. How much money was in your bank account at the start of the day?" << endl; //asking for start amount in bank account
-    cin >> bankMoney; 
-    cout << "Great. How many bank transactions were done today?" << endl; //asking for number of transactions
+    // Asking user
+    cout << "Hello. How much money was in your bank account at the start of the day?" << endl; // Asking for start amount in bank account
+    cin >> startAmount; 
+
+    cout << "Great. How many bank transactions were done today?" << endl; // Asking for number of transactions
     cin >> numberTransactions; 
 
-    //asking user for transaction type and amount spent
-    cout << "Did you use a debit or crdit card? Click 1 for debit and 2 for credit." << endl; //asking for type of transaction
-    cin >> transactionType;
-    cout << "How much was spent in this transaction?" << endl; //asking for amount spent in transaction
-    cin >> amountTransaction;   
+    // Loop
+    while (a < numberTransactions)
+    {
+        cout << "Did you use a debit or credit card? Click 1 for debit and 2 for credit." << endl; //asking for transaction type
+        cin >> transactionType;
+        
+        cout << "What value was the transaction?" << endl; //asking for transaction value
+        cin >> amountTransaction;
+        
+        if (transactionType == 1) // If the transaction is debit
+        {
+            bankMoney1 = startAmount - amountTransaction; //formula for calcualting debit
+            cout << "Total amount after debit = " << fixed << setprecision(2) << bankMoney1 << endl; 
+        }
+        else if (transactionType == 2) // If the transaction is credit
+        {
+            bankMoney2 = startAmount + amountTransaction; //formula for calculating credit
+            cout << "Total amount after credit = " << fixed << setprecision(2) << bankMoney2 << endl; 
+        }
+        else // If the transaction type is invalid
+        {
+            cout << "Invalid. Please enter 1 for debit and 2 for credit." << endl; 
+        }
+
+        a++; // Increment counter
+    }    
     
-    //while loop Option 1 
-    while (transactionType == 1) //while loop if transaction is debit (1)
-        {
-        cout << "After that transaction you have " << totalDebit << " in your bank account."; //minus amount from transaction from bank account
-        
-        cout << "Do you want to contine? Click A for yes and B for no." << endl;
-        cin >> yesOrNo;
-        
-        if (yesOrNo == 1) 
-        cout << "Did you use a debit or crdit card? Click 1 for debit and 2 for credit." << endl;
-        cin >> transactionType; 
-        
-        if (yesOrNo == 2)
-        cout << "Thank you for playing. Have a nice day." << endl;
-        }
-        
-
-    //while loop Option 2 
-    while (transactionType == 2) //while loop if transaction is credit (2)
-        {
-        cout << "After that transaction you have " << totalCredit << " in your bank account."; //add amount from transaction to bank account
-        
-        cout << "Do you want to contine? Click A for yes and B for no." << endl;
-        cin >> yesOrNo;
-        
-        if (yesOrNo == 1) 
-        cout << "Did you use a debit or crdit card? Click 1 for debit and 2 for credit." << endl;
-        cin >> transactionType; 
-        
-        if (yesOrNo == 2)
-        cout << "Thank you for playing. Have a nice day." << endl;
-        }
-  
-
-    //extra credit - print the total in the account at the end of the day  
-        cout << "You have " << (totalDebit + totalCredit) << " at the end of the day!" << endl;
-       
+    totalMoney = bankMoney1 + bankMoney2; //finding total amount at end of the day
+    cout << "Final balance = " << fixed << setprecision(2) << totalMoney << endl; //final balance = 
+    cout << "Have a nice day!" << endl;
+    
     return 0; 
 }
